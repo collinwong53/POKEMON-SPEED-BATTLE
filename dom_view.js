77b63b91985c27b803c6be8455b6f2f36432bdfa
R-Brewster <rbrewster@ucmerced.edu>
2017-10-10 15:09:10 -0700

    Basic winners modal added

---
+++ dom_view.js
@@ -0,0 +1,37 @@
+//****************************************View****************************************//
+
+/***************************************************************************************************
+ * card animation - card slides in and flips after start
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+/***************************************************************************************************
+ * button light - lights up buttons
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+/***************************************************************************************************
+ * start button - starts game, calls start game function in controller
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+/***************************************************************************************************
+ * displayWinVideo - shows a video of the winning pokemon
+ * @param  {undefined} none
+ * @returns {undefined} none
+ * @calls {undefined} none
+ */
+
+function displayWinVideo(){
+
+    this. displayVideo = function () {
+        $("#video_player").css('z-index', '+1');
+    };
+    this.displayVideo();
+}
