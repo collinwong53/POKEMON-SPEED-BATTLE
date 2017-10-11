d5de4dc266e5661b78cd33f2b1f96d6d2c329e1b
jay <jaywayez@gmail.com>
2017-10-10 18:29:33 -0700

    commit to pull anthony

--- youtube_api.js
+++ youtube_api.js
@@ -2,7 +2,7 @@
 function get_youtube_data(pokemon_name) {
 
     var promise = { //create an object that will handle the promise itself
-        then: function(resolve, reject){
+        then: function (resolve, reject) {
             this.reject = reject;
             this.resolve = resolve;
         }
@@ -16,25 +16,26 @@ function get_youtube_data(pokemon_name) {
             q: pokemon_name,
             maxResults: 10,
             type: "video"
-    },
-        success: function(data) {
+        },
+        success: function (data) {
             promise.resolve(data);  //use the promise object to trigger the success function
         },
-        error: function(){
+        error: function () {
             promise.reject('oops!'); //use the promise object to trigger the error function
         }
     });
     return promise;
-}
 
-function winner (data){
-    video_list = data;
-    var random_vid = video_list.video[Math.floor(Math.random() * video_list.video.length)].id;
-    $('#counter').addClass("player").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + random_vid + '" frameborder="0" allowfullscreen"></iframe>');
+    function winner(data) {
+        video_list = data;
+        var random_vid = video_list.video[Math.floor(Math.random() * video_list.video.length)].id;
+        $('#counter').addClass("player").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + random_vid + '" frameborder="0" allowfullscreen"></iframe>');
 
-    console.log('it worked!',data);
-}
-function failed_video(message){
-    $('#counter').addClass("player").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/BMqOLULKonM' + '" frameborder="0" allowfullscreen"></iframe>');
-    console.log(message);
+        console.log('it worked!', data);
+    }
+
+    function failed_video(message) {
+        $('#counter').addClass("player").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/BMqOLULKonM' + '" frameborder="0" allowfullscreen"></iframe>');
+        console.log(message);
+    }
 }
